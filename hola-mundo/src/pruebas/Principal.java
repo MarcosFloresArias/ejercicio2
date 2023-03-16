ec336dd33a1b86b710a6e60b2a15750885082e73
Marcosfloresarias <m.flores.arias@iescristobaldemonroy.es>
2023-03-16 08:58:16 +0100

    subido 2

--- hola-mundo/src/pruebas/Principal.java
+++ hola-mundo/src/pruebas/Principal.java
@@ -3,7 +3,9 @@ package pruebas;
 public class Principal {
 
 	public static void main(String[] args) {
+		System.out.println("Dale Wachooo");
 		System.out.println("Hola Mundo");
+		System.out.println("Dale Perrekeeee");
 	}
 
 }
